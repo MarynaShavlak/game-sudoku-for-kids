a5da948922a9e92bd4db465273e40e726f455f9f
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-07-18 18:24:55 +0300

    delete consoles

--- js/statistics.js
+++ js/statistics.js
@@ -19,8 +19,6 @@ statistics = storedStatistics ? JSON.parse(storedStatistics) : statistics;
 updateLocalStorage();
 
 function getGameTimeValue() {
-  console.log('timerID: ', timerID);
-  console.log('timer: ', timer);
   if (timerID) {
     const gameTime = timerValue[chosenLevel] - timer;
     statistics[chosenLevel].winGameTimeValues.push(gameTime);
